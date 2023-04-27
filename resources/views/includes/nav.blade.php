5befdae8b8bc3b11e29a4b0a1f58a5dae548d934
Adrian Coronel <adrianonel78@gmail.com>
2023-04-27 12:40:34 -0500

    First commit

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        Centro de Ayuda <span id="span-nav">Tecsup</span>
+        <img src="#" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      </a>
+    </div>
+  </nav>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+  <!-- FONTS GOOGLE -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+
+
+  <!-- GET BOOTSTRAP -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  @vite(['resources/css/app.css'])
+
+
+  <title>Document</title>
+</head>
+<body>
+ 
+  <header>
+    @include('includes/nav')
+  </header>
+
+
+ 
+</body>
+</html>
