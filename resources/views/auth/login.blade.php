68fe8e12310f76492c0d7ac324dcf07dd16204d2
Adrian Coronel <adrianonel78@gmail.com>
2023-05-26 19:18:28 -0500

    Descarga y configuracion de Bootstrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,15 +4,29 @@
 
 @section('content')
 
-  <div class="d-flex align-items-center" style="height: 88vh">
-    @include('auth._form',[
-    'btnText'=>'Siguiente',
-    'title'=>'Iniciar sesión',
-    'message'=>'¿Necesitas cuenta?',
-    'a'=>'Registrarse',
-    'link'=>'register'
-    ])
-  </div>
+<div class="card shadow-lg px-5 py-4 col-11  col-lg-4 col-md-6 col-sm-8 mx-auto text-center position-absolute top-50 start-50 translate-middle">
+  <h4 class="text-secondary" style="font-family: 'Poppins', sans-serif;font-weight:600;">¡Hola, <span class="text-primary">te damos la <br> bienvenida!</span></h4>
+  <form  action="">
+    <div class="form-group mt-3">
+      <input class="form-control py-3 bg-light border-0 shadow-sm" type="text" placeholder="DNI">
+    </div>
+    <div class="form-group mt-3">
+      <input class="form-control py-3 bg-light border-0 shadow-sm" type="email" placeholder="Correo">
+    </div>
+    <a href="#" style="font-size: 14px;" class="mt-5 nav-link text-secondary">Recuperar contraseña</a>
+    <div class="w-100 d-flex flex-column">  
+      <button id="btn-login" class="btn mt-3 mb-2 text-white border-0 py-2 shadow-sm" type="submit">Iniciar sesión</button>
+      <span style="font-size: 18px;" class="text-secondary">o</span>    
+      <a id="btn-access-google" href="#" class="btn btn-light my-2 py-2 shadow-sm">
+        Acceder con Google
+        <img id="icon-google" src="./img/icon-google.png" alt="">
+      </a>
+    </div>
+  </form>
+
+  <!-- LINK REGISTER -->
+  <p class="text-secondary mt-2"><a href="#">¡Registrate</a> en centro de ayuda <strong class="text-secondary">TECSUP</strong></p>
+</div>
 
 @endsection
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,27 +5,18 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-
-  <!-- FONTS GOOGLE -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
-
-
-  <!-- GET BOOTSTRAP -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-  @vite(['resources/css/app.css'])
-
+  @vite(['resources/sass/app.scss','resources/js/bootstrap.js'])
 
   <title>@yield('title')</title>
 </head>
 <body>
   <header>
-    @include('includes/nav')
+    
+    {{-- @include('includes/nav') --}}
   </header>
 
   <div class="container">
-    <div class="row">
+    <div class="row"> 
       @yield('content')
     </div>
   </div>
